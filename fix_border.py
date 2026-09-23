3f393cd0d35bb92207ce55f78440c3b31ab9ad8e
krishna <krishna.gusknp2023@ce.du.ac.in>
2026-09-23 17:31:08 +0530

    chore: remove temporary patch scripts

--- fix_border.py
+++
@@ -1,7 +0,0 @@
-import re
-with open("apps/mobile/public/styles.css", "r") as f:
-    css = f.read()
-
-# I want ONLY .avatar-face to have 20px, the rest should be 8px.
-# But wait, my previous command already changed it back to 8px everywhere because of the first sed command!
-# Let's verify what the current state is.
